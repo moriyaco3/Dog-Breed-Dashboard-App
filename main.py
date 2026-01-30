a289718fa890b2ba43a2b17a4f327fd008d6b11f
Moriya Cohen <moriyaco31@gmail.com>
2026-01-30 19:35:02 +0200

    add api key

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import requests
 
-st.title('App')
+API_KEY = st.secrets["DOG_API_KEY"]
+URL = "https://api.thedogapi.com/v1/breeds"
+HEADERS = {"x-api-key": API_KEY}
 
 name = st.text_input('Enter your name', '')
 if name:
